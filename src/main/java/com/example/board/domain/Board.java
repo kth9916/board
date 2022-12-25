6f602acdf0b4400096a5290dc768d4e43a18a307
kth9916 <rlaxogudsla2@gmail.com>
2022-12-26 04:32:27 +0900

    feat : 게시판 생성 및 몽고 디비 연결 , 프론트 엔드 동기화

---
+++ src/main/java/com/example/board/domain/Board.java
@@ -0,0 +1,14 @@
+package com.example.board.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "board")
+public class Board {
+    @Id
+    private String _id;
+    private String title;
+    private String content;
+}
